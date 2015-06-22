d6ed5071b891c40f7a02afb119d3f0013fe6e347
altamiranoErica <altamirano.erica@gmail.com>
2015-06-21 22:41:24 -0300

    Agregado un test de prueba

--- spec/mainCtrlSpec.js
+++ spec/mainCtrlSpec.js
@@ -12,13 +12,30 @@ describe("Main Controller", function() {
 
     beforeEach(inject(function ($rootScope, $controller) {
         scope = $rootScope.$new();
+
+        var factory_auth = {
+            isLoggedIn: function() { return true; }
+        };
+
+        var factory_trip = {
+            trips: [],
+            getAll: function() {
+                this.trips = ["Trip1", "Trip2", "Trip3"];
+                return this.trips;
+            }
+        };
+
+        factory_trip.getAll();
+
         MainController = $controller('MainCtrl', {
-            $scope: scope
+            $scope: scope,
+            trips: factory_trip,
+            auth: factory_auth
         });
     }));
 
-    it('says hello world!', function () {
-        expect(scope.predicate).toEqual("-name");
+    it('fetch all trips', function () {
+        expect(scope.trips).toEqual(["Trip1", "Trip2", "Trip3"]);
     });
 
 });
